b93d02777953b5ed1cedc74252392741f470abd4
LaaLkA <n.afonchenkov@yandex.ru>
2024-10-30 14:36:51 +0300

    lect completed

--- src/main/java/org/example/Db.java
+++ src/main/java/org/example/Db.java
@@ -1,27 +1,27 @@
 package org.example;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
-import java.sql.*;
+import java.util.List;
+import java.util.Queue;
 
 public class Db {
-    private static final String URL = "jdbc:mysql://localhost:3306";
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
     private static final String USER = "root";
     private static final String PASSWORD = "admin";
 
-
     public static void con() {
+        Connector connector = new Connector();
 
-        try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            Statement statement = con.createStatement();
-            statement.execute("DROP SCHEMA test;");
-            statement.execute("CREATE SCHEMA test;");
-            statement.execute("CREATE TABLE test.table (id INT NOT NULL, firstname VARCHAR(45) NULL, lastname VARCHAR(45) NULL, PRIMARY KEY (id));");
-            statement.execute("INSERT INTO test.table (id, firstname, lastname)\n" +
-                    " VALUES (1,'Иванов', 'Иван')");
-            //19:33
-
-            ResultSet set = statement.executeQuery("SELECT * FROM test.table;");
-        }catch (SQLException e){
-            throw new RuntimeException(e);
+        try (Session session = connector.getSession()){
+            Transaction t = session.beginTransaction();
+            List<Magic> magics = session.createQuery("FROM Magic", Magic.class).getResultList();
+            magics.forEach(session::delete);
+            t.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
